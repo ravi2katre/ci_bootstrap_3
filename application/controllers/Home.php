0e25270919ae3a51e208d5b8cb58839ed48c5659
ravi2katre <ravi2katre@gmail.com>
2018-08-14 21:35:46 +0530

    ok

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -9,8 +9,18 @@ class Home extends MY_Controller {
 	public function index()
 	{
 	    //echo "fddfdfdf";
-        $this->system_message->add_success("nnnnnnnnnnnnn");
+        //$this->system_message->add_success("nnnnnnnnnnnnn");
+        //$this->system_message->set_success("nnnnnnnnnnnnn");
 
 		$this->render('home', 'full_width');
 	}
+
+    public function home_msg()
+    {
+        //echo "fddfdfdf";
+        //$this->system_message->add_success("nnnnnnnnnnnnn");
+        $this->system_message->set_success("nnnnnnnnnnnnn");
+redirect("home/index");
+
+    }
 }
--- application/views/home.php
+++ application/views/home.php
@@ -1,4 +1,5 @@
 
 <div class="container">
+    <?php echo $this->system_message->render(); ?>
 	<p>view file: /application/views/home.php</p>
 </div>
